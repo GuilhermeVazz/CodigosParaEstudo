d93454aadae587d3ffbca6c8bf60b9df3fa62b2f
GuilhermeVazz <62060346+GuilhermeVazz@users.noreply.github.com>
2020-07-21 08:52:37 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -58,6 +58,107 @@ const menu = function (decisao) {
             }
             console.log(piloto)
             break;
+         
+        case 8:
+            function classificarTriangulo(lado1, lado2, lado3) {
+
+                if (lado1 == lado2 && lado2 == lado3) {
+                    return 'Equilátero'
+                } else if (lado1 == lado2 || lado2 == lado3 || lado1 == lado3) {
+                    return 'Isóceles'
+                } else {
+                    return 'Escaleno'
+                }
+            }
+            console.log(classificarTriangulo(2,2,2))
+            console.log(classificarTriangulo(2,3,3))
+            console.log(classificarTriangulo(2,3,4))
+            break
+
+        case 9:
+            function sacarDinheiro(valorSaque) {
+                let contador100 = 0
+                let contador50 = 0
+                let contador10 = 0
+                let contador5 = 0
+                let contador2 = 0;
+                let valorNota = calcularValorNota(valorSaque)
+
+                while (valorSaque >= valorNota) {
+                    switch (valorNota) {
+                        case 100:
+                            valorSaque -= 100
+                            contador100++
+                            break
+                        case 50:
+                            valorSaque -= 50
+                            contador50++
+                            break
+                        case 10:
+                            valorSaque -= 10
+                            contador10++
+                            break
+                        case 5:
+                            valorSaque -= 5
+                            contador5++
+                            break
+                        case 2:
+                            valorSaque -= 2
+                            contador2++
+                            break
+                    }
+
+                    valorNota = calcularValorNota(valorSaque)
+                }
+                return elaborarResultado(contador100, contador50, contador10, contador5, contador2)
+            }
+            function calcularValorNota(valorSaque) {
+                if (valorSaque >= 100) {
+                    return 100
+                } else if (valorSaque >= 50) {
+                    return 50
+                } else if (valorSaque >= 10) {
+                    return 10
+                } else if (valorSaque >= 5) {
+                    return 5
+                } else if (valorSaque >= 2) {
+                    return 2
+                }
+            }
+
+            function elaborarResultado(contador100, contador50, contador10, contador5, contador2) {
+
+                let resultado = ''
+
+                if (contador100 > 0) {
+
+                    resultado += `${contador100} nota(s) de R$ 100. `
+                }
+
+                if (contador50 > 0) {
+
+                    resultado += `${contador50} nota(s) de R$ 50.`
+                }
+
+                if (contador10 > 0) {
+
+                    resultado += `${contador10} notas(s) de R$ 10.`
+                }
+
+                if (contador5 > 0) {
+
+                    resultado += `${contador5} notas(s) de R$ 5.`
+                }
+
+                if (contador2 > 0) {
+
+                    resultado += `${contador2} notas(s) de R$ 2.`
+                }
+                return resultado
+            }
+
+            console.log(sacarDinheiro(156))
+            break
         
         default:
             console.log("Try Again")
